3f47af64aa458904f270dbde9bf57f228f9df810
keller10101010 <kellergarcia16@gmail.com>
2024-08-15 13:51:52 -0600

    sacar los meses  de los años que tengo
    insertar y cambiar datos

--- primer_proyecto sc/Program.cs
+++ primer_proyecto sc/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!")
+﻿using System;
+
+public class program
+{
+
+    static void Main(string[] args)
+    {
+
+        
+
+        Console.WriteLine("Bienvenidos a nuestro pirmer sistema creado en C#");
+
+        Console.WriteLine("Ingresa tu nombre para continuar");
+        string Nombre = Console.ReadLine();//Para recibir valores de caracter
+        Console.WriteLine("Ingrese su edad");// Para recibir la edad 
+        int Edad=Convert.ToInt32(Console.ReadLine());// Convertir a dato tipo double
+        double EdadMeses = Edad * 12;// Calculos Matematicos 
+        Console.WriteLine(Nombre +" tu edad en Meses es: " + EdadMeses + "Meses");//Concatenacion
+        if (Edad > 18)
+        {
+        Console.WriteLine("Usted es Mayor de edad");
+        }
+        else
+        {
+            Console.WriteLine("Usted es Menor de edad");
+        }
+    
+    
+    
+    }
+
+
+}
